b0fa42b97e46362b4d91ed541ae162801fa6c41a
Sanju094 <118598803+Sanju094@users.noreply.github.com>
2023-10-27 18:59:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
 API.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category',categoryRoutes);
